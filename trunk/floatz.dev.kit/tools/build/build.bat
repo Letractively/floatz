9be8f95559147695899682cd2ecc740ebc65bc9e
hhumml <hhumml@bacccd10-0802-11df-8f5a-9d0a8e3f0dce>
2010-03-06 09:24:16 +0000

    Adding build script to \tools adding shrinked version of floatz.js to \templates

---
+++ trunk/floatz.dev.kit/tools/build/build.bat
@@ -0,0 +1,38 @@
+@echo off
+
+cd ..\shrink
+
+echo.
+echo Building floatz.fixed ...
+shrink /p:../build/floatz.fixed.profile /o:../../../floatz/floatz.fixed.css
+
+echo.
+echo Building floatz.liquid ...
+shrink /p:../build/floatz.liquid.profile /o:../../../floatz/floatz.liquid.css
+
+echo.
+echo Building floatz.fixed ...
+shrink /p:../build/floatz.center.profile /o:../../../floatz/floatz.center.css
+
+echo.
+echo Updating templates ...
+echo floatz.fixed.css
+copy ..\..\..\floatz\floatz.fixed.css  ..\..\templates\layout.010.fixed\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.fixed.css  ..\..\templates\layout.011.fixed\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.fixed.css  ..\..\templates\layout.111.fixed\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.fixed.css  ..\..\templates\layout.110.fixed\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.fixed.css  ..\..\templates\layout.empty\styles\floatz.1.1.0\ > nul
+echo floatz.liquid.css
+copy ..\..\..\floatz\floatz.liquid.css  ..\..\templates\layout.010.liquid\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.liquid.css  ..\..\templates\layout.011.liquid\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.liquid.css  ..\..\templates\layout.111.liquid\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.liquid.css  ..\..\templates\layout.110.liquid\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.liquid.css  ..\..\templates\layout.empty\styles\floatz.1.1.0\ > nul
+echo floatz.center.css
+copy ..\..\..\floatz\floatz.center.css  ..\..\templates\layout.010.center\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.center.css  ..\..\templates\layout.011.center\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.center.css  ..\..\templates\layout.111.center\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.center.css  ..\..\templates\layout.110.center\styles\floatz.1.1.0\ > nul
+copy ..\..\..\floatz\floatz.center.css  ..\..\templates\layout.empty\styles\floatz.1.1.0\ > nul
+
+cd ../build
