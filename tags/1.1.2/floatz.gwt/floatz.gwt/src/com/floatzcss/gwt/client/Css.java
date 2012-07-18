3edd1316903e25e1cf6e598f8c69d61a170f3fe7
hhumml@gmail.com <hhumml@gmail.com@bacccd10-0802-11df-8f5a-9d0a8e3f0dce>
2012-07-18 07:10:36 +0000

    Version 1.1.2

---
+++ tags/1.1.2/floatz.gwt/floatz.gwt/src/com/floatzcss/gwt/client/Css.java
@@ -0,0 +1,32 @@
+package com.floatzcss.gwt.client;
+
+import com.floatzcss.gwt.client.resource.Floatz;
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Module entry point for resource bundle
+ * <p>
+ * Copyright (c) 1998-2012 by :humml:design
+ * http://design.humml.eu/toolbox/floatz Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * </p>
+ * <p>
+ * See: http://code.google.com/p/floatz/
+ * </p>
+ * @author Harald Humml
+ * @version 1.1.2
+ */
+
+public class Css implements EntryPoint {
+
+	// Entry point method
+	public void onModuleLoad() {
+
+		// Load all floatz css bundles in the necessary order
+		Floatz.INSTANCE.resetMeyer().ensureInjected();
+		Floatz.INSTANCE.reset().ensureInjected();
+		Floatz.INSTANCE.layout().ensureInjected();
+		Floatz.INSTANCE.form().ensureInjected();
+		Floatz.INSTANCE.nav().ensureInjected();
+	}
+}
