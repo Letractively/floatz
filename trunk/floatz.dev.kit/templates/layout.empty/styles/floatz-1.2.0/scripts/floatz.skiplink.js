e7eb5f89d999a1c69f34d634755f7e9c709bebaf
hhumml@gmail.com <hhumml@gmail.com@bacccd10-0802-11df-8f5a-9d0a8e3f0dce>
2013-03-03 16:10:22 +0000

    Preparations for version 1.2.0

--- trunk/floatz.dev.kit/templates/layout.empty/styles/floatz-1.2.0/scripts/floatz.skiplink.js
+++ trunk/floatz.dev.kit/templates/layout.empty/styles/floatz-1.2.0/scripts/floatz.skiplink.js
@@ -1,3 +1,6 @@
+/* floatz CSS Framework v1.2.0
+   Copyright (c) 1998-2013 by :humml:design
+   Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0 */
 
 (function(){var self={module:{name:"floatz.skiplink",version:"1.2.0",start:start}};var module=self.module;function start(){fixSkipLinks();fixSkipLinkAnchors();floatz.log(floatz.LOGLEVEL.INFO,"Module "+module.name+" started",module.name);}
 function fixSkipLinks(){if(!floatz.browser.isMobile()&&(floatz.browser.webkit||floatz.browser.opera)){floatz.log(floatz.LOGLEVEL.DEBUG,"Adding click handler that sets focus to all flz_skipnav a",module.name);$(".flz_skipnav a").click(function(){$(this.href.substring(this.href.indexOf("#"),this.href.length)).focus();});}}
--- trunk/floatz.dev.kit/tools/build/build.sh
+++ trunk/floatz.dev.kit/tools/build/build.sh
@@ -7,10 +7,18 @@ cleanup() {
 	then 
 		rm *.css.tmp
 	fi
-	#if [ -f *.js.tmp ] 
-	#then 
-	#	rm *.js.tmp
-	#fi
+	if [ -f *.js.tmp ] 
+	then 
+		rm *.js.tmp
+	fi
+	if [ -d ../../../floatz.dev.kit-$FLOATZ_VERSION ]
+	then
+		rm -rf ../../../floatz.dev.kit-$FLOATZ_VERSION
+	fi
+	if [ -d ../../../floatz.dev.kit-$FLOATZ_VERSION ]
+	then
+		rm -rf ../../../floatz-$FLOATZ_VERSION
+	fi
 }
 
 function updateTemplate() {
@@ -47,6 +55,12 @@ function updateTemplate() {
 	cp ../../NOTICE.txt ../../templates/$template/styles/floatz-$FLOATZ_VERSION/
 }
 
+function createFile() {
+	echo "/* floatz CSS Framework v$FLOATZ_VERSION" > $1
+	echo "   Copyright (c) 1998-2013 by :humml:design" >> $1
+	echo "   Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0 */" >> $1
+}
+
 # -------------------------- Script body -------------------------
 #stop on first error 
 set -e 
@@ -67,7 +81,8 @@ if [ -x $LESSC ]
 then
 	#Create minified css
 	echo "floatz.build | INFO  | Creating minified version of floatz.fixed.css"
-	lessc --compress ../../src/floatz.reset.meyer.css > floatz.fixed.css.tmp
+	createFile floatz.fixed.css.tmp
+	lessc --compress ../../src/floatz.reset.meyer.css >> floatz.fixed.css.tmp
 	lessc --compress ../../src/floatz.reset.css >> floatz.fixed.css.tmp
 	lessc --compress ../../src/floatz.layout.css >> floatz.fixed.css.tmp
 	lessc --compress ../../src/floatz.nav.css >> floatz.fixed.css.tmp
@@ -75,7 +90,8 @@ then
 	mv floatz.fixed.css.tmp ../../../floatz/floatz.fixed.css
 
 	echo "floatz.build | INFO  | Creating minified version of floatz.liquid.css"
-	lessc --compress ../../src/floatz.reset.meyer.css > floatz.liquid.css.tmp
+	createFile floatz.liquid.css.tmp
+	lessc --compress ../../src/floatz.reset.meyer.css >> floatz.liquid.css.tmp
 	lessc --compress ../../src/floatz.reset.css >> floatz.liquid.css.tmp
 	lessc --compress ../../src/floatz.layout.css >> floatz.liquid.css.tmp
 	lessc --compress ../../src/floatz.layout.liquid.css >> floatz.liquid.css.tmp
@@ -84,14 +100,14 @@ then
 	mv floatz.liquid.css.tmp ../../../floatz/floatz.liquid.css
 
 	echo "floatz.build | INFO  | Creating minified version of floatz.center.css"
-	lessc --compress ../../src/floatz.reset.meyer.css > floatz.center.css.tmp
+	createFile floatz.center.css.tmp
+	lessc --compress ../../src/floatz.reset.meyer.css >> floatz.center.css.tmp
 	lessc --compress ../../src/floatz.reset.css >> floatz.center.css.tmp
 	lessc --compress ../../src/floatz.layout.css >> floatz.center.css.tmp
 	lessc --compress ../../src/floatz.layout.center.css >> floatz.center.css.tmp
 	lessc --compress ../../src/floatz.nav.css >> floatz.center.css.tmp
 	lessc --compress ../../src/floatz.form.css >> floatz.center.css.tmp
 	mv floatz.center.css.tmp ../../../floatz/floatz.center.css
-
 else
 	echo "floatz.build | ERROR | Less compiler could not be found. Please install less compiler before build."
 fi
@@ -100,20 +116,31 @@ if [ -x $JSMIN ]
 then
 	#Create minified js
 	echo "floatz.build | INFO  | Creating minified version of floatz.js"
-	../jsmin/jsmin < ../../src/scripts/floatz.js > floatz.js.tmp
+	createFile floatz.js.tmp
+	../jsmin/jsmin < ../../src/scripts/floatz.js >> floatz.js.tmp
 	mv floatz.js.tmp ../../../floatz/scripts/floatz.js
 
 	echo "floatz.build | INFO  | Creating minified version of floatz.browser.js"
-	../jsmin/jsmin < ../../src/scripts/floatz.browser.js > floatz.browser.js.tmp
+	createFile floatz.browser.js.tmp
+	../jsmin/jsmin < ../../src/scripts/floatz.browser.js >> floatz.browser.js.tmp
 	mv floatz.browser.js.tmp ../../../floatz/scripts/floatz.browser.js
 
 	echo "floatz.build | INFO  | Creating minified version of floatz.skiplink.js"
-	../jsmin/jsmin < ../../src/scripts/floatz.skiplink.js > floatz.skiplink.js.tmp
+	createFile floatz.skiplink.js.tmp
+	../jsmin/jsmin < ../../src/scripts/floatz.skiplink.js >> floatz.skiplink.js.tmp
 	mv floatz.skiplink.js.tmp ../../../floatz/scripts/floatz.skiplink.js
 
 	echo "floatz.build | INFO  | Creating minified version of floatz.mobile.js"
-	../jsmin/jsmin < ../../src/scripts/floatz.mobile.js > floatz.mobile.js.tmp
+	createFile floatz.mobile.js.tmp
+	../jsmin/jsmin < ../../src/scripts/floatz.mobile.js >> floatz.mobile.js.tmp
 	mv floatz.mobile.js.tmp ../../../floatz/scripts/floatz.mobile.js
+
+	echo "floatz.build | INFO  | Updating floatz related files"
+	cp ../../src/scripts/jquery-$JQUERY_VERSION.min.js ../../../floatz/scripts/
+	cp ../../src/scripts/LAB-$LAB_VERSION.min.js ../../../floatz/scripts/
+	cp ../../src/scripts/less-$LESS_VERSION.min.js ../../../floatz/scripts/
+	cp ../../NOTICE.txt ../../../floatz/	
+	cp ../../src/images/* ../../../floatz/images	
 else
 	echo "floatz.build | ERROR | Jsmin compiler could not be found. Please get full floatz.dev.kit and retry."
 fi
@@ -138,3 +165,20 @@ updateTemplate "center" "010"
 updateTemplate "center" "011"
 updateTemplate "center" "110"
 updateTemplate "center" "111"
+
+#Create floatz distribution packages
+echo "floatz.build | INFO  | Creating floatz.dev.kit-$FLOATZ_VERSION.zip"
+mkdir ../../../floatz.dev.kit-$FLOATZ_VERSION
+cp -r ../../* ../../../floatz.dev.kit-$FLOATZ_VERSION/
+rm -rf ../../../floatz.dev.kit-$FLOATZ_VERSION/sandbox
+rm -rf "../../../floatz.dev.kit-$FLOATZ_VERSION/samples/Basic Concepts/images"
+rm -rf "../../../floatz.dev.kit-$FLOATZ_VERSION/samples/Layouting Content/images"
+rm -rf "../../../floatz.dev.kit-$FLOATZ_VERSION/samples/Layouting Forms/images"
+rm -rf "../../../floatz.dev.kit-$FLOATZ_VERSION/samples/Layouting Navigation/images"
+rm -rf "../../../floatz.dev.kit-$FLOATZ_VERSION/samples/Layouting Pages/images"
+zip -rmq ../../../floatz.dev.kit-$FLOATZ_VERSION.zip ../../../floatz.dev.kit-$FLOATZ_VERSION
+
+echo "floatz.build | INFO  | Creating floatz-$FLOATZ_VERSION.zip"
+mkdir ../../../floatz-$FLOATZ_VERSION
+cp -r ../../../floatz/* ../../../floatz-$FLOATZ_VERSION/
+zip -rmq ../../../floatz-$FLOATZ_VERSION.zip ../../../floatz-$FLOATZ_VERSION
